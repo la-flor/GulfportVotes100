1c9a9f4003585d414f1ae080e3d1076d159d81e5
lafBot <64456835+lafBot@users.noreply.github.com>
2020-10-19 11:51:41 -0600

    Initial project framework and runnable server

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///gulfport_votes'
+app.config['SECRET_KEY'] = 'Super_Secret_TO_Be_Changed'
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/events')
+def events_page():
+    return render_template('events.html')
+
+@app.route('/social')
+def social_page():
+    return render_template('social.html')
+
+@app.route('/voting')
+def voting_page():
+    return render_template('voting.html')
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Getting Started:  
+- **createdb gulfport_votes** for server to be able to be run.
+- while in venv: pip3 install -r requirements.txt
+
+# Future Thoughts:
+Implimentation of database:
+- Username/password
+- Adding calendar events
+- Blog
+- Documents for recreation of "____ Votes 100%" in other towns/cities
