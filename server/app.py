0ec78de92f450634e9e2f23e89901c9e6d589fd5
lafBot <64456835+lafBot@users.noreply.github.com>
2020-10-28 22:56:49 -0600

    Updated client names, removed unneeded server files, and started to impliment auth for admin

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-from models import db, connect_db
+from admin import db, Admin, connect_db
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -14,18 +14,10 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
-@app.route('/')
-def home_page():
-    return render_template('home.html')
-
-@app.route('/events')
-def events_page():
-    return render_template('events.html')
-
-@app.route('/social')
-def social_page():
-    return render_template('social.html')
-
-@app.route('/voting')
-def voting_page():
-    return render_template('voting.html')
+@app.route('/admin', methods=['POST'])
+def admin_login():
+    username = request.form['email']
+    password = request.form['password']
+    
+    # import pdb; pdb.set_trace()
+    return jsonify({ "status": "successful" })
