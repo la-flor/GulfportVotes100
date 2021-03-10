4a56d2a91b8138484fcb783d8e346f3fc5915c7a
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-10 16:56:00 -0700

    Incorporated flask admin routes and login capabilities

--- server/app.py
+++ server/app.py
@@ -1,14 +1,15 @@
 import os
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, redirect, jsonify, flash, render_template
+from flask_cors import CORS
 from flask_debugtoolbar import DebugToolbarExtension
 
-from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin
-
+from forms import LoginForm, CreateUserForm
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
-from models import User, Event, db, connect_db
+from models import User, Event, MyModelView, db, connect_db
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "you should have a password in your config file")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///gulfport_votes')
@@ -20,9 +21,12 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
+# app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
 # instatiate and create admin view to edit events database table
 admin = Admin(app)
-admin.add_view(ModelView(Event, db.session))
+admin.add_view(MyModelView(Event, db.session))
+admin.add_view(MyModelView(User, db.session))
 
 # instantiate and initialize login manager
 login_manager = LoginManager()
@@ -43,19 +47,21 @@ def home():
 
 @app.route('/createuser', methods=['GET', 'POST'])
 def create_user():
-    if request.method == 'POST':
+    form = CreateUserForm()
+    if form.validate_on_submit():
         try:
-            user = User.create_user(request.json.get('username'), request.json.get('password'))
+            user = User.create_user(form.username.data, form.password.data)
 
             if not user:
                 raise AssertionError("Unable to insert user into database.")
             
             login_user(user)
 
-            return jsonify({ "status": "New user successfully created."})
+            return redirect("/admin")
         except:
-            return jsonify({ "status": "Unsuccessful create user attempt."})
-    return jsonify({ "status": "Welcome to the create new user page."})
+            flash("Invalid create user attempt.")
+            # return redirect('/createuser')
+    return render_template("create_user.html", form=form)
 
 
 # GET request returns login page with login
@@ -63,26 +69,34 @@ def create_user():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        flash("You're already logged in.  You may logout or use the links on the toolbar to navigate.")
         return redirect('/admin')
-    if request.method == 'POST':
+    
+    form = LoginForm()
+    if form.validate_on_submit():
         try:
-            username = request.json.get('username')
-            password = request.json.get('password')
+            username = form.username.data
+            password = form.password.data
 
             user = User.is_valid(username=username, password=password)
             login_user(user)
             
-            return jsonify({ "status": "Login successful"})
+            return redirect("/admin")
         except:
-            return jsonify({ "status": "Unsuccessful login attempt."})
+            flash("Invalid login attempt.")
+            return redirect('/login')
 
-    return jsonify({ "status": "Login page successfully loaded."})
+    return render_template('login.html', form=form)
 
 @app.route("/logout")
-@login_required
 def logout():
-    logout_user()
-    return jsonify({ "status": "Successfully logged out."})
+    if current_user.is_authenticated:
+        logout_user()
+        flash("You have successfully logged out.")
+        return redirect('/admin')
+    else:
+        flash("You are not currently logged in.  To login, click the 'Login' button for further access.")
+        return redirect('/admin')
 
 @app.route("/events", methods=["GET", "POST"])
 def events():
