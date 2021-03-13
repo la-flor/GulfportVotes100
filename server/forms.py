044549c7438944b8e140f648fc4d9f415e085de0
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-12 19:44:46 -0700

    Added forms for resetting password

--- server/forms.py
+++ server/forms.py
@@ -1,11 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, DataRequired, Email, EqualTo, ValidationError
+from models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=6, max=25)])
+    email = StringField('Email', validators=[InputRequired(), Length(min=6, max=25)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=25)])
     
 class CreateUserForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=6, max=25)])
+    email = StringField('Email', validators=[InputRequired(), Length(min=6, max=25)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=25)])
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError("There is no account with that email.")
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password",
+                                        validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Reset Password")
